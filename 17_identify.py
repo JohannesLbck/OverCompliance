48679c61638fee0b9f44520932c3bb2f464f4a84
JohannesLbck <loebbeckejohannes@hotmail.de>
2025-07-11 14:51:50 +0200

    Cleaned up scripts slightly, updated README

--- 17_identify.py
+++ 17_identify.py
@@ -1,54 +1,57 @@
 import pandas as pd
+import argparse
 
-def verify(C,I):
+def verify(C,D):
     Compliant = False
     Per = False
     if C:
-        if I:
+        if D:
             Compliant = True
             Per = True
     else:
         Compliant = True
-    return Compliant, C, I, Per
+    return Compliant, C, D, Per
 
 def verify_B2(trace_df):
     labels = trace_df['concept:name'].tolist()
     C1 = (trace_df['CreditScore'] < 100).any()
     C2 = (trace_df['case:RequestedAmount'] > 40000).any()
     C = C1 and C2
-    #I = "A_Denied" in trace_df['concept:name'].values
-    I = 'W_Assess potential fraud' in labels
-    ## I am looking for some label that happens often but not all the time
-    #I = 'W_Assess potential fraud' in df['concept:name'].values 
-    return verify(C, I)
+    D = 'W_Assess potential fraud' in labels
+    return verify(C, D)
 
 def verify_B3(trace_df):
     labels = trace_df['concept:name'].tolist()
     C = 'A_Submitted' in labels 
-    #I = "A_Denied" in trace_df['concept:name'].values
-    I = 'W_Complete application' in labels
-    ## I am looking for some label that happens often but not all the time
-    #I = 'W_Assess potential fraud' in df['concept:name'].values 
-    return verify(C, I)
+    D = 'W_Complete application' in labels
+    return verify(C, D)
 
 def verify_B4(trace_df):
     labels = trace_df['concept:name'].tolist()
-    I= 'W_Complete application' in labels
-    #I = "A_Denied" in trace_df['concept:name'].values
+    D= 'W_Complete application' in labels
     C = 'O_Sent (mail and online)' in labels
-    ## I am looking for some label that happens often but not all the time
-    #I = 'W_Assess potential fraud' in df['concept:name'].values 
-    return verify(C, I)
+    return verify(C, D)
 
 def verify_B5(trace_df):
     labels = df['concept:name'].tolist()
     C = (trace_df['case:LoanGoal'] == 'Home improvement').any()
-    I = (trace_df['case:RequestedAmount'] < 10000).any()
-    #I = "A_Denied" in trace_df['concept:name'].values
-    ## I am looking for some label that happens often but not all the time
-    #I = 'W_Assess potential fraud' in df['concept:name'].values
-    return verify(C, I)
+    D = (trace_df['case:RequestedAmount'] < 10000).any()
+    return verify(C, D)
 
+
+verifiers = {
+    'B6' : verify_B2,
+    'B7' : verify_B3,
+    'B8' : verify_B4,
+    'B9' : verify_B5
+        }
+
+
+## ArgsParsing
+parser = argparse.ArgumentParser(description="Evaluate a specific requirement of the BPIC17 dataset")
+parser.add_argument('--r', type=str, default='B6', choices=['B6', 'B7', 'B8', 'B9'], help= 'Which requirement to evaluate')
+
+args = parser.parse_args()
 
 ## Loading
 df = pd.read_csv('DataSets/Real/BPI_17.csv', sep='\t')
@@ -65,13 +68,13 @@ print(len(grouped))
 
 Set_Compliant = []
 Set_C = []
-Set_I = []
+Set_D = []
 Set_Per = []
 for _, trace in grouped:
-    compliant, c, i, per = verify_B2(trace)
+    compliant, c, i, per = verifiers[args.r](trace)
     Set_Compliant.append(compliant)
     Set_C.append(c)
-    Set_I.append(i)
+    Set_D.append(i)
     Set_Per.append(per)
 
 print(f'The process has {len(grouped)} traces')
@@ -79,9 +82,9 @@ print(f'The process has {len(grouped)} traces')
 max_over_compliance = (1-(sum(Set_C)/len(grouped)))
 print(f'The process has {sum(Set_Compliant)} compliant traces (|Compliant|)')
 print(f'The process requires the execution of the consequence in {sum(Set_C)} traces (|C|)')
-print(f'The process is executing the consequence in {sum(Set_I)} traces (|I|)')
+print(f'The process is executing the consequence in {sum(Set_D)} traces (|D|)')
 print(f'The process is correctly executing the consequence in {sum(Set_Per)} traces (|Per|)')
-over_compliance_level = ((sum(Set_I)-sum(Set_Per))/len(grouped))
+over_compliance_level = ((sum(Set_D)-sum(Set_Per))/len(grouped))
 print(f'The over-compliance level of the process is {over_compliance_level*100}%')
 print(f'The maximal over-compliance level of the process is {max_over_compliance*100}%')
 under_compliance_level = ((sum(Set_Per)-sum(Set_C))/len(grouped))
@@ -103,7 +106,7 @@ C_con = 120
 P_over = 0
 print(f'Assuming: C_viol = {C_viol}, C_con = {C_con}, P_over = {P_over}')
 p = sum(Set_C)/len(grouped)
-c = sum(Set_I)/len(grouped)
+c = sum(Set_D)/len(grouped)
 u = round(1-(sum(Set_Compliant)/len(grouped)),2)
 print(sum(Set_Compliant))
 print(len(grouped))
--- discrete_levels.py
+++ discrete_levels.py
@@ -11,7 +11,7 @@ data = load_all('DataSets/Mixed')
 
 Set_Compliant = []
 Set_C = []
-Set_I = []
+Set_D = []
 Set_Per = []
 Set_C_5000 = []
 Set_C_50000 = []
@@ -19,7 +19,7 @@ for trace in data:
     compliant, c, i, per, c_5000, c_50000 = r2_discrete_levels(trace)
     Set_Compliant.append(compliant)
     Set_C.append(c)
-    Set_I.append(i)
+    Set_D.append(i)
     Set_Per.append(per)
     Set_C_5000.append(c_5000)
     Set_C_50000.append(c_50000)
@@ -31,9 +31,9 @@ print(f'In the Process {sum(Set_C_5000)} traces are credit worthy and above 5000
 print(f'In the Process {sum(Set_C_50000)} traces are credit worthy and above 50000')
 print(f'The process has {sum(Set_Compliant)} compliant traces (|Compliant|)')
 print(f'The process requires the execution of the consequence in {sum(Set_C)} traces (|C|)')
-print(f'The process is executing the consequence in {sum(Set_I)} traces (|I|)')
+print(f'The process is executing the consequence in {sum(Set_D)} traces (|D|)')
 print(f'The process is correctly executing the consequence in {sum(Set_Per)} traces (|Per|)')
-over_compliance_level = ((sum(Set_I)-sum(Set_C))/len(data))
+over_compliance_level = ((sum(Set_D)-sum(Set_C))/len(data))
 print(f'The over-compliance level of the process is {over_compliance_level*100}%')
 print(f'The maximal over-compliance level of the process is {max_over_compliance*100}%')
 under_compliance_level = ((sum(Set_Per)-sum(Set_C))/len(data))
@@ -55,7 +55,7 @@ C_con = 120
 P_over = 0
 print(f'Assuming: C_viol = {C_viol}, C_con = {C_con}, P_over = {P_over}')
 p = sum(Set_C)/len(data)
-c = sum(Set_I)/len(data)
+c = sum(Set_D)/len(data)
 u = round(1-(sum(Set_Compliant)/len(data)),2)
 print(sum(Set_Compliant))
 print(len(data))
